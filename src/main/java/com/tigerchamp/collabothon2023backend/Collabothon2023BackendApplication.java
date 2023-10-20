782a083d5545b1ec28617d508d20c37dadf18183
Chono Chonov <chonochonovuk@gmail.com>
2023-10-20 15:15:56 +0300

    Spring with MongoDB

---
+++ src/main/java/com/tigerchamp/collabothon2023backend/Collabothon2023BackendApplication.java
@@ -0,0 +1,13 @@
+package com.tigerchamp.collabothon2023backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Collabothon2023BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Collabothon2023BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tigerchamp/collabothon2023backend/Collabothon2023BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tigerchamp.collabothon2023backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Collabothon2023BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
